f9b6672b0b8bd595483df861bb634ff00fb8147c
Mutangana Edgar <mutanganaedgar@Mutanganas-MacBook.local>
2018-02-16 10:23:21 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//1. add event listeners to every element in the html and mark each box with an id.{1,2,3,4,5,6,7,8,9}
+//2. create the game logic
+  //21.chose players
+  //22.note player 1:x and player 2:o
+  //23.begin listenining to moves at nine boxes
+  //24.once x has played switch option to player o and vice versa
+  //25.after each move check for wins and losses
+  //26.check win
+  //27.if no win move to step 23
+  //28.if thier is a win annouce winner
+  //29.terminate game
+  //210.store results in local storage 
+  //211.display records for the current game
+  //212.restart game
+
+  
